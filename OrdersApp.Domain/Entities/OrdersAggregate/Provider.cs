8f19ba164e433aa74437cdef4680ad73d0280d1b
Вячеслав Литвиненко <ptb.ikc@yandex.ru>
2022-11-08 18:37:14 +0500

    DAL, Domain - основные сущности

---
+++ OrdersApp.Domain/Entities/OrdersAggregate/Provider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdersApp.Domain.Entities.OrdersAggregate
+{
+    class Provider : BaseEntity
+    {
+    }
+}
